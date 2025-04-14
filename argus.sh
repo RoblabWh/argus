dc0f2e14c4e808fa633a4d62c94e23d69d9038f7
Niklas Digakis <niklas.digakis@studmail.w-hs.de>
2025-04-14 14:09:59 +0200

    split gpu.yml files and add ollama as optional container, also build stella_vslam with all available threads

--- argus.sh
+++ argus.sh
@@ -45,20 +45,40 @@ fi
 set -e
 
 # add docker compose overlay yml for gpu if detected
-docker_compose="docker compose -f $ARGUS_PATH/docker-compose.yml -f $ARGUS_PATH/docker-compose.nodeodm.yml -f $ARGUS_PATH/docker-compose.webodm.yml"
+docker_compose="docker compose -f $ARGUS_PATH/docker-compose.argus.yml -f $ARGUS_PATH/docker-compose.nodeodm.yml -f $ARGUS_PATH/docker-compose.webodm.yml"
+if $ARGUS_STELLA_VSLAM; then
+	echo "StellaVSLAM is Enabled"
+	docker_compose="$docker_compose -f $ARGUS_PATH/docker-compose.stella_vslam.yml"
+else
+	echo "StellaVSLAM is Disabled"
+fi
+if $ARGUS_OLLAMA; then
+	echo "Ollama is Enabled"
+	docker_compose="$docker_compose -f $ARGUS_PATH/docker-compose.ollama.yml"
+else
+	echo "Ollama is Disabled"
+fi
 if $ARGUS_GPU_NVIDIA; then
 	echo "Using NVIDIA GPU"
-	docker_compose="$docker_compose -f $ARGUS_PATH/docker-compose.gpu.nvidia.yml"
+	docker_compose="$docker_compose -f $ARGUS_PATH/docker-compose.argus.nvidia.yml -f $ARGUS_PATH/docker-compose.nodeodm.nvidia.yml"
+	if $ARGUS_OLLAMA; then
+		docker_compose="$docker_compose -f $ARGUS_PATH/docker-compose.ollama.nvidia.yml"
+	fi
+	if $ARGUS_STELLA_VSLAM; then
+		docker_compose="$docker_compose -f $ARGUS_PATH/docker-compose.stella_vslam.nvidia.yml"
+	fi
 elif $ARGUS_GPU_INTEL; then
 	echo "Using INTEL GPU"
-	docker_compose="$docker_compose -f $ARGUS_PATH/docker-compose.gpu.intel.yml"
+	docker_compose="$docker_compose -f $ARGUS_PATH/docker-compose.nodeodm.intel.yml"
 elif $ARGUS_GPU_AMD; then
 	echo "Using AMD GPU"
-	echo "NodeODM has no support for AMD GPU at the moment, falling back to CPU"
+	if $ARGUS_OLLAMA; then
+		docker_compose="$docker_compose -f $ARGUS_PATH/docker-compose.ollama.amd.yml"
+	fi
 else
 	echo "Using CPU"
 fi
 
 # run docker compose with provided arguments
-echo "Runing command: $docker_compose $@"
+echo "Runing command: $docker_compose $*"
 $docker_compose "$@"
